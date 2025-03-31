6393790ea2017bcb4e43d960b79e03fa34245429
Taiyakis <ezerskas.mindaugas@gmail.com>
2025-03-31 23:23:48 +0300

    visual indicator of API status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, Input, input, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, NgStyle],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  statusName = '';
+  statusColor = {}
 
+  @Input() set statusIndicator(value: string) {
+    this.setStatus(value);
+  }
+
+  setStatus(status: string) {
+    switch (status) {
+      case 'error':
+        this.setStatusColor('#ff1313')
+        break;
+      case 'pending':
+        this.setStatusColor('#f5f50d')
+        break;
+      case 'success':
+        this.setStatusColor('#0cf30c')
+        break;
+      default:
+        break;
+    }
+  }
+
+  setStatusColor(color: string) {
+    this.statusColor = {
+      'background-color': color
+    }
+  }
 }
