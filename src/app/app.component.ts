94f9841da37f203c3b948e2232c6a354f57347fa
Taiyakis <ezerskas.mindaugas@gmail.com>
2025-12-18 12:10:35 +0200

    Added snowing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { catchError, fromEvent, retry, throwError, timer } from 'rxjs';
 import { NgIf } from '@angular/common';
 import { ProgressSpinnerMode, MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
+import { SnowComponent } from "./snow/snow.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RosterComponent, RaidInfoComponent, NgIf, MatProgressSpinnerModule, MatIconModule],
+  imports: [HeaderComponent, RosterComponent, RaidInfoComponent, NgIf, MatProgressSpinnerModule, MatIconModule, SnowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ApiService]
